fc6759d62a7615ad8f2d660880064350991dde9e
Jeremie Lim <hello@jeremiespoken.com>
2017-08-20 16:28:38 -0700

    Add initial scripts for locomotion

---
+++ Obey/Assets/Scripts/Locomotion/FirstPersonAgentController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FirstPersonAgentController : MonoBehaviour
+{
+    public bool relativeMovement = true;
+
+    protected NavMeshAgent agent;
+
+    public void Awake ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    public void Update ()
+    {
+        if ( !agent.enabled )
+        {
+            return;
+        }
+
+        Vector3 delta = new Vector3( Input.GetAxis( "Horizontal" ), 0.0f,  Input.GetAxis( "Vertical" ) );
+
+        if ( delta != Vector3.zero )
+        {
+            //use our forward and right to move in a relative direction
+            if ( relativeMovement )
+            {
+                delta = delta.x * transform.right + delta.z * transform.forward;
+            }
+
+            agent.destination = transform.position + delta;
+        }
+    }
+}
