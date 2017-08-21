9cca29bdeb66807edd01a5eba0c83f6d244edeac
Jeremie Lim <hello@jeremiespoken.com>
2017-08-20 19:37:09 -0700

    Add basic movement scripts

---
+++ Obey/Assets/Scripts/Locomotion/NavMeshPatroller.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshPatroller : MonoBehaviour
+{
+    public List<Vector3> patrolPoints = new List<Vector3>();
+    RaycastHit hitInfo = new RaycastHit();
+    protected int pointIndex;
+    protected NavMeshAgent agent;
+
+    public void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Start()
+    {
+        pointIndex = 0;
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                patrolPoints.Add(hit.point);
+            }
+        }
+
+        // check if there are navigable points
+        if (patrolPoints.Count > 0)
+        {
+            // patrol to target
+            PatrolToPoint(pointIndex);
+        }
+
+        // check if object has reached patrol point then move to the next one
+        if (agent.remainingDistance <= 0.0f)
+        {
+            if (pointIndex + 1 < patrolPoints.Count)
+            {
+                pointIndex += 1;
+            }
+        }
+    }
+
+    public void PatrolToPoint(int curIndexPoint)
+    {
+        agent.destination = patrolPoints[curIndexPoint];
+    }
+}
